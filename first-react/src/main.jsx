51584523a7c25211a275d29f0161c939f0d9b81d
Waqas Tariq <waqastariqdar@gmail.com>
2024-11-21 08:47:57 +0500

    basic app react native with blank template

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -3,20 +3,19 @@ import { createRoot } from 'react-dom/client'
 import LifecycleDemo from './examples/LifeCycleDemo'
 import TestAPI from './examples/TestAPI'
 import Counter from './examples/Counter'
-//import App from './App.jsx'
-//import './index.css'
+import App from './App.jsx'
+import './index.css'
 //import MyApp from './examples/MyApp.jsx'
 
-/*
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App /> 
-    <MyApp />    
+    <LifecycleDemo />       
   </StrictMode>,
 
-)*/
-
-createRoot(document.getElementById('root')).render(
-  
-  <Counter />
 )
+
+// createRoot(document.getElementById('root')).render(
+  
+//   <Counter />
+// )
